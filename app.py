c16000f10d070be4fb74142767a0edf30bacad54
HillaryNyakundi <hillarynyakundi66@gmail.com>
2024-02-14 16:07:42 +0300

    Added the log in page

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 
-
 def login():
     username = st.sidebar.text_input("Username")
     password = st.sidebar.text_input("Password", type="password")
@@ -16,17 +15,15 @@ def login():
             st.error("Invalid username or password")
             return False
 
-
 def main():
     if login():
-        page = st.sidebar.selectbox(
-            "Explore or Predict", ("Predict", "Explore"))
+        page = st.sidebar.selectbox("Explore or Predict", ("Predict", "Explore"))
 
         if page == "Predict":
             show_predict_page()
         else:
             show_explore_page()
 
-
 if __name__ == "__main__":
     main()
+
