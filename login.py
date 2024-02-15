23a2dd415eff8c7ca3b7be36ab84ab479e31d828
HillaryNyakundi <hillarynyakundi66@gmail.com>
2024-02-15 07:00:00 +0300

    Made tiny changes

--- login.py
+++ login.py
@@ -5,22 +5,19 @@ from app import session_state
 def show_login_page():
     st.title("Login")
 
-    if not session_state.logged_in:
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
 
-        if st.button("Login"):
-            # Your authentication logic here
-            # Here, you can validate the username and password
-            # For simplicity, we'll just assume any non-empty username and password are valid
-            if username and password:
-                session_state.logged_in = True
-                session_state.username = username
-                session_state.password = password
-            else:
-                st.error("Invalid username or password")
-    else:
-        st.write("You are already logged in.")
+    if st.button("Login"):
+        # Your authentication logic here
+        # Here, you can validate the username and password
+        # For simplicity, we'll just assume any non-empty username and password are valid
+        if username and password:
+            session_state.logged_in = True
+            session_state.username = username
+            session_state.password = password
+        else:
+            st.error("Invalid username or password")
 
 
 if __name__ == "__main__":
